5b87db234fefe518a3e8864d89f656b202671edd
Garrett <lamgarr@outlook.com>
2023-11-05 21:04:27 -0800

    Add Input component

---
+++ client/src/components/InputComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import inputPropInterface from "../utils/interfaces/inputPropInterface";
+import "../styles/InputComponent.css";
+
+function InputComponent() {
+    return(
+        <div></div>
+    );
+}
+
+export default InputComponent;
