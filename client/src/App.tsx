6837d75650a89083aa58a707c0a342e3fcb442f1
Garrett <lamgarr@outlook.com>
2023-11-06 21:16:37 -0800

    Add Landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import SignupPage from './pages/SignupPage';
+import LandingPage from './pages/LandingPage';
 import OverlayComponent from './components/OverlayComponent';
 import './styles/App.css';
 
@@ -18,6 +19,7 @@ function App() {
     <BrowserRouter>
       <OverlayComponent isOpen={overlayIsOpen}/>
       <Routes>
+        <Route path="/:username" element={<LandingPage/>}></Route>
         <Route path="/signup" element={<SignupPage/>}></Route>
         <Route path="/login" element={<LoginPage/>}></Route>
         <Route path="/" element={<HomePage/>}></Route>
---
+++ client/src/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import landingPageInterface from "../utils/interfaces/landingPageInterface";
+import "../styles/LandingPage.css";
+
+function LandingPage() {
+    return(
+        <main></main>
+    );
+}
+
+export default LandingPage;
