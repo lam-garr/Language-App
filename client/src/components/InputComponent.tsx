bd3c3615b4a342f464c973674c52cbc87df9e0e3
Garrett <lamgarr@outlook.com>
2023-11-07 22:24:41 -0800

    Edit Input component

--- client/src/components/InputComponent.tsx
+++ client/src/components/InputComponent.tsx
@@ -2,9 +2,20 @@ import React from "react";
 import inputPropInterface from "../utils/interfaces/inputPropInterface";
 import "../styles/InputComponent.css";
 
-function InputComponent() {
+function InputComponent(prop: inputPropInterface) {
     return(
-        <div></div>
+        <div className={`inputField ${prop.value !==""?"field_not_empty":""}`}>
+            <div className="fieldLabel">
+                <label htmlFor={prop.type}>
+                    {prop.placeholder.toLowerCase()}
+                </label>
+                <span className="err">
+                    {prop.errorMessage}
+                </span>
+            </div>
+            <input required={prop.requried} className="fieldInput" placeholder={prop.placeholder}
+            type={prop.type} value={prop.value} onChange={prop.onChangeHandler}></input>
+        </div>
     );
 }
 
--- client/src/utils/interfaces/inputPropInterface.ts
+++ client/src/utils/interfaces/inputPropInterface.ts
@@ -1,3 +1,10 @@
+import React from "react";
+
 export default interface inputPropInterface {
-    
+    onChangeHandler: (event: React.ChangeEvent<HTMLInputElement>) => void,
+    value: string,
+    type: string,
+    placeholder: string,
+    errorMessage: string,
+    requried: boolean;
 }
