5d08be4e7546e406a089c31ba5768362a4017c7f
Garrett <lamgarr@outlook.com>
2023-11-08 19:34:27 -0800

    Edit Home page

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,9 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import InputComponent from "../components/InputComponent";
 import "../styles/LoginPage.css";
 
 function LoginPage() {
+
+    const navigate = useNavigate();
+
+    const [ loginInput, setLoginInput ] = useState("");
+
+    const handleLoginChange = (e:any) => {
+        setLoginInput(e.target.value);
+    }
+
+    const [ passwordInput, setPasswordInput ] = useState("");
+
+    const handlePasswordChange = (e:any) => {
+        setPasswordInput(e.target.value);
+    }
+
+    const [ loginErrMessage, setLoginErrMessage ] = useState("");
+
+    const handleLoginErr = (message: string) => {
+        setLoginErrMessage(message);
+    }
+
+    const [ passwordErrMessage, setPasswordErrMessage ] = useState("");
+
+    const handlePasswordErr = (message: string) => {
+        setPasswordErrMessage(message);
+    }
+
+    const [ apiErr , setApiErr ] = useState(false);
+
+    const [ fetching, setFetching ] = useState(false);
+
+    const handleSubmit = async (e:any) => {
+        e.preventDefault();
+
+        //check if input is valid
+        if(loginInput.length <= 1) {
+            handleLoginErr("username invalid");
+            return;
+        }
+
+        setLoginErrMessage("");
+
+        //check if password id valid
+        if(passwordInput.length <= 1) {
+            handlePasswordErr("password invalid");
+            return;
+        }
+
+        setPasswordErrMessage("");
+
+        setFetching(true);
+
+        const handleLoading = () => {
+            if(fetching) {
+                return "Loading";
+            } else {
+                return "Log In";
+            }
+        }
+
+        //call login api
+        /* const loginData = await fetch("/api/login", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({username: loginInput, password: passwordInput})
+        });
+
+        const apiDataObj = await loginData.json();
+
+        if(!apiDataObj.response) {
+            setLoginInput("");
+            setPasswordInput("");
+            setApiErr(true);
+        }else {
+            setApiErr(false);
+            //set return token from api to localStorage
+            window.localStorage.setItem("AccessToken", JSON.stringify(apiDataObj.accessToken));
+            setFetching(false);
+            navigate("/{username}");
+        } */
+    }
+
     return(
-        <main></main>
+        <main>
+            <div className="log-in-content">
+                <h1>Login</h1>
+                <form onSubmit={handleSubmit}>
+                    <InputComponent onChangeHandler={handleLoginChange} value={loginInput} type="text"
+                     placeholder="Username" errorMessage={loginErrMessage} requried={true}/>
+                    <InputComponent onChangeHandler={handlePasswordChange} value={passwordInput} type="password"
+                     placeholder="Password" errorMessage={passwordErrMessage} requried={true}/>
+                    <div className="log-in-submit">
+                        {apiErr && <span className="log-in-err">Error logging in, please try again.</span>}
+                        <button className="log-in-submitBtn" type="submit" disabled={fetching}>Login</button>
+                    </div>
+                </form>
+                <div className="log-in-members">
+                    Not a member? <Link to="/signup">Sign Up</Link>
+                </div>
+            </div>
+        </main>
     )
 }
 
