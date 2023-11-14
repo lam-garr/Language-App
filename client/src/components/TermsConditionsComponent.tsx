fbc8ea72d868bd18be247ae1823b02c121b3e681
Garrett <lamgarr@outlook.com>
2023-11-13 21:36:40 -0800

    Add Terms & Conditions component

---
+++ client/src/components/TermsConditionsComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import termsConditionsInterface from "../utils/interfaces/termsConditionsInterface";
+import "../styles/TermsConditions.css";
+
+function TermsConditionsComponent(prop: termsConditionsInterface) {
+    return(
+        <aside></aside>
+    );
+}
+
+export default TermsConditionsComponent;
