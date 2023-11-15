8827a4b66caa078f389fa26f05cf7d0d37746039
Garrett <lamgarr@outlook.com>
2023-11-14 21:45:42 -0800

    Add useRef to handle closing terms & conditions modal

--- client/src/components/TermsConditionsComponent.tsx
+++ client/src/components/TermsConditionsComponent.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import termsConditionsInterface from "../utils/interfaces/termsConditionsInterface";
 import "../styles/TermsConditions.css";
 
 function TermsConditionsComponent(prop: termsConditionsInterface) {
+
+    const modalRef = useRef<any>(null);
+
+    useEffect(() => {
+        const handleEvent = (e:any) => {
+            if((!modalRef.current.contains(e.target)) && prop.modalIsOpen) {
+                prop.closeModal();
+            }
+        }
+
+        document.addEventListener("mousedown", handleEvent);
+
+        return() => {document.removeEventListener("mousedown", handleEvent)};
+    })
+    
     return(
-        <aside></aside>
+        <aside ref={modalRef}></aside>
     );
 }
 
--- client/src/utils/interfaces/termsConditionsInterface.ts
+++ client/src/utils/interfaces/termsConditionsInterface.ts
@@ -1,3 +1,7 @@
+import React from "react";
+
 export default interface termsConditionsInterface {
-    
+    modalHandler: (event: React.MouseEvent<HTMLButtonElement>) => void,
+    modalIsOpen: boolean,
+    closeModal: () => void
 }
