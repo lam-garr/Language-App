d91b7bd70ec7b7fda6e427ecf53354a319a15c75
Garrett <lamgarr@outlook.com>
2023-11-21 22:13:32 -0800

    Edit Home page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -3,7 +3,24 @@ import "../styles/HomePage.css";
 
 function HomePage() {
     return(
-        <main></main>
+        <main className="home-content">
+            <header className="home-header">
+                <div className="home-header-container">
+                </div>
+            </header>
+            <section className="home-section-one">
+                <div className="home-sec-one-container">
+                </div>
+            </section>
+            <section className="home-section-two">
+                <div className="home-sec-two-container">
+                </div>
+            </section>
+            <section className="home-section-three">
+                <div className="home-sec-three-container">
+                </div>
+            </section>
+        </main>
     );
 }
 
