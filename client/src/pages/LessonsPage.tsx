1ac6ca4ff2ca6914fdb7b9ce46c406b9b0773c1e
Garrett <lamgarr@outlook.com>
2023-11-22 22:26:20 -0800

    Edit Lessons page

--- client/src/pages/LessonsPage.tsx
+++ client/src/pages/LessonsPage.tsx
@@ -3,7 +3,22 @@ import "../styles/LessonsPage.css";
 
 function LessonsPage() {
     return(
-        <main></main>
+        <main className="lessons-content">
+            <section className="lessons-section-one">
+                <div className="lessons-sec-one-title">
+                    Lessons
+                </div>
+            </section>
+            <section className="lessons-section-two">
+                {true ? 
+                (<div className="lessons-sec-two-content">
+                    
+                </div>):
+                (<div className="lessons-empty">
+                    <span>There's nothing here, start a course to get started!</span>
+                </div>)}
+            </section>
+        </main>
     );
 }
 
