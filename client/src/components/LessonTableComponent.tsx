a0c7e9cd88d34c6bb2c6ac19bee82da62a2013f3
Garrett <lamgarr@outlook.com>
2023-11-25 22:24:13 -0800

    Add Lesson Table component

---
+++ client/src/components/LessonTableComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import lessonTblComponentInterface from "../utils/interfaces/lessonTblComponentInterface";
+import "../styles/LessonTableComponent.css";
+
+function LessonTableComponent() {
+    return(
+        <table></table>
+    );
+}
+
+export default LessonTableComponent;
