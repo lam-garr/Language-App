9736e691359bec27a03a19ad10d7275fa7ce4574
Garrett <lamgarr@outlook.com>
2023-11-26 21:30:10 -0800

    Add handling to populate table data in Lesson Table component

--- client/src/components/LessonTableComponent.tsx
+++ client/src/components/LessonTableComponent.tsx
@@ -3,9 +3,26 @@ import { Link } from "react-router-dom";
 import lessonTblComponentInterface from "../utils/interfaces/lessonTblComponentInterface";
 import "../styles/LessonTableComponent.css";
 
-function LessonTableComponent() {
+function LessonTableComponent(prop: lessonTblComponentInterface) {
     return(
-        <table></table>
+        <table className="lesson-tbl">
+            <tbody>
+                <tr className="lesson-tbl-top">
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>View</th>
+                </tr>
+                {prop.lessonData.map(lesson => {
+                    return(
+                        <tr>
+                            <td>{lesson.id}</td>
+                            <td>{lesson.title}</td>
+                            <td><Link to={`/user/`}><button className="lesson-tbl-view-btn">View</button></Link></td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
     );
 }
 
--- client/src/utils/interfaces/lessonTblComponentInterface.ts
+++ client/src/utils/interfaces/lessonTblComponentInterface.ts
@@ -1,3 +1,3 @@
 export default interface lessonTblComponentInterface {
-    
+    lessonData: any[];//
 }
