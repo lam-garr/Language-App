191b81ec7330eab182c04a23a874305eb96b58a9
Garrett <lamgarr@outlook.com>
2023-11-29 21:10:08 -0800

    Edit Courses page

--- client/src/pages/CoursesPage.tsx
+++ client/src/pages/CoursesPage.tsx
@@ -3,7 +3,18 @@ import "../styles/CoursesPage.css";
 
 function CoursesPage() {
     return(
-        <main></main>
+        <main className="courses-main-content">
+            <section className="courses-section-one">
+                <div className="courses-sec-one-title">
+                    <h1>Courses</h1>
+                </div>
+            </section>
+            <section className="courses-section-two">
+                <div className="courses-sec-two-course-options">
+                {/*  */}
+                </div>
+            </section>
+        </main>
     );
 }
 
