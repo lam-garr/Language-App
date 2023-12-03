7a9759644d870ac4579c6cd68229b8fe014a6633
Garrett <lamgarr@outlook.com>
2023-12-02 21:55:45 -0800

    Add Courses Table component

---
+++ client/src/components/CoursesTableComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import coursesTblComponentInterface from "../utils/interfaces/coursesTblComponentInterface";
+import "../styles/CoursesTableComponent.css";
+
+function CoursesTableComponent() {
+    return(
+        <table></table>
+    );
+}
+
+export default CoursesTableComponent;
