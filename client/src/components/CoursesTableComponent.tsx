09f711f185afd8057830a45a48217343b495301d
Garrett <lamgarr@outlook.com>
2023-12-03 21:32:40 -0800

    Edit Courses Table component

--- client/src/components/CoursesTableComponent.tsx
+++ client/src/components/CoursesTableComponent.tsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import coursesTblComponentInterface from "../utils/interfaces/coursesTblComponentInterface";
 import "../styles/CoursesTableComponent.css";
 
-function CoursesTableComponent() {
+function CoursesTableComponent(prop: coursesTblComponentInterface) {
     return(
-        <table></table>
+        <table className="course-tbl">
+            <tbody>
+                <tr className="course-tbl-top">
+                    <th>Title</th>
+                    <th></th>
+                </tr>
+                {prop.courseData.map(lesson => {
+                    return(
+                        <tr>
+                            <td>{lesson.title}</td>
+                            <td><Link to={`/`}><button className="course-tbl-view-btn">Start Lesson</button></Link></td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
     );
 }
 
