61955e6e56a61616e776b3832c506321745fd203
Garrett <lamgarr@outlook.com>
2023-12-09 20:31:30 -0800

    Edit Sidebar component

--- client/src/components/SidebarComponent.tsx
+++ client/src/components/SidebarComponent.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import sidebarComponentPropInterface from "../utils/interfaces/sidebarComponentPropInterface";
 import "../styles/SidebarComponent.css";
+import { Link } from "react-router-dom";
 
-function SidebarComponent() {
+function SidebarComponent(prop: sidebarComponentPropInterface) {
     return(
-        <aside></aside>
+        <aside className={`sidebar ${prop.sidebarIsOpen?"active":"inactive"}`}>
+            <div className="sidebar-header">
+                <div></div>
+                <button className="sidebar-close-btn" onClick={prop.clickHandler}>&times;</button>
+            </div>
+            <Link to={"/"}><button onClick={prop.closeHandler}>Home</button></Link>
+            <Link to={"/"}><button onClick={prop.closeHandler}>Account</button></Link>
+        </aside>
     );
 }
 
--- client/src/utils/interfaces/sidebarComponentPropInterface.ts
+++ client/src/utils/interfaces/sidebarComponentPropInterface.ts
@@ -1,3 +1,7 @@
+import React from "react";
+
 export default interface sidebarComponentPropInterface {
-    
+    clickHandler: (event: React.MouseEvent<HTMLButtonElement>) => void,
+    closeHandler: () => void,
+    sidebarIsOpen: boolean;
 }
