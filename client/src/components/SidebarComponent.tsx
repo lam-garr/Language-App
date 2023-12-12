4b37f3283fc2cdad5bfa9058c3026346d960a713
Garrett <lamgarr@outlook.com>
2023-12-11 21:03:01 -0800

    Add handling to close Sidebar component with useRef

--- client/src/components/SidebarComponent.tsx
+++ client/src/components/SidebarComponent.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import sidebarComponentPropInterface from "../utils/interfaces/sidebarComponentPropInterface";
 import "../styles/SidebarComponent.css";
 import { Link } from "react-router-dom";
 
 function SidebarComponent(prop: sidebarComponentPropInterface) {
+
+    const sidebarRef = useRef<any>(null);
+
+    useEffect(() => {
+        const handleEvent = (e:any) => {
+            if((!sidebarRef.current.contains(e.target)) && prop.sidebarIsOpen) {
+                prop.closeHandler();
+            }
+        };
+
+        document.addEventListener("mousedown", handleEvent);
+
+        return() => {
+            document.removeEventListener("mousedown", handleEvent);
+        }
+    })
+
     return(
-        <aside className={`sidebar ${prop.sidebarIsOpen?"active":"inactive"}`}>
+        <aside className={`sidebar ${prop.sidebarIsOpen?"active":"inactive"}`} ref={sidebarRef}>
             <div className="sidebar-header">
                 <div></div>
                 <button className="sidebar-close-btn" onClick={prop.clickHandler}>&times;</button>
