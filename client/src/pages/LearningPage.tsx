e680020a91f1e3d97d729ff232dd29e18eede904
Garrett <lamgarr@outlook.com>
2023-12-14 21:25:33 -0800

    Add elements to Learning page

--- client/src/pages/LearningPage.tsx
+++ client/src/pages/LearningPage.tsx
@@ -3,7 +3,27 @@ import "../styles/LearningPage.css";
 
 function LearningPage() {
     return(
-        <main></main>
+        <main className="learning-main">
+            <div className="learning-content">
+                <section className="learning-sec-one">
+                    <div className="learning-sec-one-title">
+                        <span className="learning-sOne-title-id">0.0</span>
+                        <span className="learning-sOne-title-name">title</span>
+                    </div>
+                </section>
+                <section className="learning-sec-two">
+                    <div className="learning-sec-two-content">
+                        content
+                    </div>
+                </section>
+                <section className="learning-sec-three">
+                    <div className="learning-sec-three-content">
+                        <button className="learning-sThree-left-btn">left</button>
+                        <button className="learning-sThree-right-btn">right</button>
+                    </div>
+                </section>
+            </div>
+        </main>
     );
 }
 
