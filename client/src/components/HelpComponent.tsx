2c2c91501dd4a43b175d2f2da161fb11e5c36cb6
Garrett <lamgarr@outlook.com>
2023-12-18 20:47:48 -0800

    Add Help modal component

---
+++ client/src/components/HelpComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import helpComponentPropInterface from "../utils/interfaces/helpComponentPropInterface";
+import "../styles/HelpComponent.css";
+
+function HelpComponent(prop: helpComponentPropInterface) {
+    return(
+        <aside></aside>
+    );
+}
+
+export default HelpComponent;
