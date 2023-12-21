b6710fb0f08ffb04195fb6d844957db130c7c37a
Garrett <lamgarr@outlook.com>
2023-12-20 21:09:48 -0800

    Add handling to close Help component with useRef

--- client/src/components/HelpComponent.tsx
+++ client/src/components/HelpComponent.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import helpComponentPropInterface from "../utils/interfaces/helpComponentPropInterface";
 import "../styles/HelpComponent.css";
 
 function HelpComponent(prop: helpComponentPropInterface) {
+
+    const modalRef = useRef<any>(null);
+
+    useEffect(() => {
+        const handleEvent = (e:any) => {
+            if((!modalRef.current.contains(e.target)) && prop.helpComponentIsOpen) {
+                prop.closeHelpComponent();
+            }
+        }
+
+        document.addEventListener("mousedown", handleEvent);
+
+        return() => {
+            document.removeEventListener("mousedown", handleEvent);
+        }
+    })
+
     return(
-        <aside className={`help-component ${prop.helpComponentIsOpen?"active":""}`}>
+        <aside className={`help-component ${prop.helpComponentIsOpen?"active":""}`} ref={modalRef}>
             <div className="help-component-header">
                 <div></div>
                 <h1 className="help-component-title">!</h1>
--- client/src/utils/interfaces/helpComponentPropInterface.ts
+++ client/src/utils/interfaces/helpComponentPropInterface.ts
@@ -3,5 +3,6 @@ import React from "react";
 export default interface helpComponentPropInterface {
     helpComponentHandler: (event: React.MouseEvent<HTMLButtonElement>) => void,
     helpComponentIsOpen: boolean,
+    closeHelpComponent: () => void,
     helpMessage: string;
 }
