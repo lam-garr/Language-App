bcbed1485a4ea3e8263aab3cd8bbbc32aeda25d4
Garrett <lamgarr@outlook.com>
2023-12-25 21:25:25 -0800

    Add api call to handle if user is already logged in

--- client/src/pages/AuthedPage.tsx
+++ client/src/pages/AuthedPage.tsx
@@ -1,9 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 function AuthedPage() {
 
     const [ validated, setValidated ] = useState(false);
+
+    const [ fetching, setFetching ] = useState(true);
+
+    useEffect(() => {
+        const validate = async () => {
+            const data = window.localStorage.getItem("AccessToken");
+
+            let dataToken;
+
+            if(data) {
+                dataToken = JSON.parse(data);
+            }
+
+            //wip
+            const response = await fetch("/api/", {
+                method:"GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${dataToken}`
+                }
+            });
+
+            const resObj = await response.json();
+
+            if(resObj) {
+                setValidated(true);
+                setFetching(false);
+            } else {
+                setFetching(false);
+            }
+        }
+
+        validate();
+    }, [])
+
+    if(fetching) {
+        return null;
+    }
 
     return validated ? <Navigate to="/:username"/> : <Outlet/>;
 }
