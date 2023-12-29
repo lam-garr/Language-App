640a3721be65d403ea93a02da5fc01177d929b2a
Garrett <lamgarr@outlook.com>
2023-12-28 18:07:21 -0800

    Add elements to Home page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -10,14 +10,36 @@ function HomePage() {
             </header>
             <section className="home-section-one">
                 <div className="home-sec-one-container">
+                    <div className="home-secOne-description">
+
+                    </div>
+                    <div className="home-secOne-imgs">
+
+                    </div>
                 </div>
             </section>
             <section className="home-section-two">
                 <div className="home-sec-two-container">
+                    <div className="home-secTwo-description">
+                        
+                    </div>
+                    <div className="home-secTwo-cards">
+                        <div className="home-secTwo-cardOne"></div>
+                        <div className="home-secTwo-cardTwo"></div>
+                        <div className="home-secTwo-cardThree"></div>
+                    </div>
                 </div>
             </section>
             <section className="home-section-three">
                 <div className="home-sec-three-container">
+                    <div className="home-secThree-description">
+                        <div className="home-secThree-title">
+
+                        </div>
+                        <div className="home-secThree-content">
+
+                        </div>
+                    </div>
                 </div>
             </section>
         </main>
